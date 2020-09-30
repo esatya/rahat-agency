355557b7384f50d2032fb8f4292e23f558c84872
Santosh <santosh@rumsan.com>
2020-09-30 17:20:08 +0545

    Clearing the repository

--- client/js/user/userRole.panel.js
+++
@@ -1,6 +0,0 @@
-import { Panel } from "rumsan-ui";
-import Service from "./service";
-
-class UserRolePanel extends Panel {}
-
-export default UserRolePanel;
--- modules/ctp/ctp.routes.ui.js
+++
@@ -1,9 +0,0 @@
-const router = require("express").Router();
-
-router.get("/", (req, res, next) => {
-  res.render("ctp/index", {
-    title: "book List "
-  });
-});
-
-module.exports = router;
