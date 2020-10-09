a1c6e39c32a8572aed6676735c41e77f62cdddd4
Binod Chaudhary <binod@rumsan.com>
2020-10-09 08:49:29 +0545

    vendors list filter and ui fixes

--- src/reducers/vendorReducer.js
+++ src/reducers/vendorReducer.js
@@ -4,13 +4,14 @@ export default (state, action) => {
   switch (action.type) {
     case `${ACTION.LIST}`:
       return {
-        ...state, // beneficiary_list, aid
+        ...state, 
         list: action.data.data,
         pagination: {
           limit: parseInt(action.data.limit),
           start: parseInt(action.data.start),
           total: parseInt(action.data.total),
-          page: parseInt(action.data.page),
+          currentPage: parseInt(action.data.page),
+          totalPages: Math.ceil(action.data.total / action.data.limit),
         },
         query: { name: action.data.name, phone: action.data.phone },
       };
