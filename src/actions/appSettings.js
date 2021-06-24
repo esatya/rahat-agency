bddcc988c7a2ed2f6003cbc4c5b1b624e6f54dec
Manjik Shrestha <manjik.shrestha@gmail.com>
2021-06-24 15:54:14 +0545

    setup web-wallet

--- src/actions/appSettings.js
+++ src/actions/appSettings.js
@@ -2,5 +2,7 @@ module.exports = {
   GET_APP_SUCCESS: 'GET_APP_SUCCESS',
   SET_TEMP_IDENTITY: 'set_temp_identity',
   SET_HASWALLET: 'set_haswallet',
-	SET_WALLET: 'set_wallet',
+  SET_WALLET: 'set_wallet',
+  SET_APP_PASSCODE:'set_app_passcode',
+  CHANGE_ISVERIFIED:'CHANGE_ISVERIFIED'
 };
