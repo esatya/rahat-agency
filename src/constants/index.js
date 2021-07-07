adf18bbdcb6339e9c3fcf49bd45c077399fee563
Binod Chaudhary <thebinod7@gmail.com>
2021-07-07 21:46:29 +0545

    bulk token issue to beneficiary

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,14 +1,24 @@
 module.exports = {
-  APP_CONSTANTS: {
-    PASSCODE_LENGTH: 6
-  },
-  
-  DEFAULT_TOKEN: {
-    NAME: 'Ether',
-    SYMBOL: 'ETH'
-  },
-  BACKUP: {
-    PASSPHRASE_RULE: '"^(?=.*[a-zA-Z])(?=.*[0-9])(?=.{12,})"',
-    GDRIVE_FOLDERNAME: 'RumsanWalletBackups'
-  }
+	APP_CONSTANTS: {
+		PASSCODE_LENGTH: 6
+	},
+
+	DEFAULT_TOKEN: {
+		NAME: 'Ether',
+		SYMBOL: 'ETH'
+	},
+	BACKUP: {
+		PASSPHRASE_RULE: '"^(?=.*[a-zA-Z])(?=.*[0-9])(?=.{12,})"',
+		GDRIVE_FOLDERNAME: 'RumsanWalletBackups'
+	},
+	TOAST: {
+		ERROR: {
+			appearance: 'error',
+			autoDismiss: true
+		},
+		SUCCESS: {
+			appearance: 'success',
+			autoDismiss: true
+		}
+	}
 };
