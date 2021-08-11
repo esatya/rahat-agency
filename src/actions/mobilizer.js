bd158d9438edbc9a15b9538b42ab340ef0e312d3
Manjik Shrestha <manjik.shrestha@gmail.com>
2021-08-11 19:36:42 +0545

    mobilizer CRUD

---
+++ src/actions/mobilizer.js
@@ -0,0 +1,13 @@
+module.exports = {
+  LIST: "LIST_MOBILIZER",
+  LIST_AID: "LIST_AID",
+  SET_AID: "SET_AID",
+  SET_MOBILIZER: "SET_MOBILIZER",
+  ADD: "ADD_MOBILIZER",
+  REMOVE: "REMOVE_MOBILIZER",
+  UPDATE: "UPDATE_MOBILIZER",
+  GET: "GET_MOBILIZER",
+  SET_LOADING: "set_mobilizer_loading",
+  RESET_LOADING: "reset_mobilizer_reset_loading",
+  MOBILIZER_TX: "MOBILIZER_TX_HISTORY",
+};
