6fdd404b8b7a7dab9b9f150796c379a66af75764
Binod Chaudhary <thebinod7@gmail.com>
2021-11-10 09:24:04 +0545

    pagination in project list

--- src/constants/index.js
+++ src/constants/index.js
@@ -3,7 +3,7 @@ module.exports = {
 		PASSCODE_LENGTH: 6,
 		BULK_BENEFICIARY_LIMIT: 200,
 		FETCH_LIMIT: 100,
-		PAGE_LIMIT: 10
+		PAGE_LIMIT: 1
 	},
 	ROLES: { ADMIN: 'Admin', MANAGER: 'Manager' },
 	DEFAULT_TOKEN: {
