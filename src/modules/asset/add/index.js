e52fbf6a88759191ecfa8b9ff624f33684d4eb49
Binod Chaudhary <thebinod7@gmail.com>
2021-11-12 14:45:02 +0545

    cleanup nft package UI

--- src/modules/asset/add/index.js
+++ src/modules/asset/add/index.js
@@ -1,19 +1,11 @@
 import React, { useState } from 'react';
-import {
-	Card,
-	CardBody,
-	FormGroup,
-	Label,
-	Input,
-	InputGroup,
-	InputGroupButtonDropdown,
-	DropdownToggle,
-	DropdownMenu,
-	DropdownItem
-} from 'reactstrap';
+import { Card, CardBody, FormGroup, Label, Input, InputGroup } from 'reactstrap';
+import { useToasts } from 'react-toast-notifications';
+
 import BreadCrumb from '../../ui_components/breadcrumb';
-import { blobToBase64 } from '../../../utils';
+import { blobToBase64, generateUID } from '../../../utils';
 import UploadPlaceholder from '../../../assets/images/download.png';
+import { TOAST } from '../../../constants';
 import Select from 'react-select';
 
 const options = [
@@ -23,33 +15,29 @@ const options = [
 ];
 
 export default function NewAsset({ match }) {
+	const { addToast } = useToasts();
 	const { projectId } = match.params;
+
 	const [formData, setFormData] = useState({
 		name: '',
 		quantity: '',
-		description: '',
-		address: '',
-		address_temporary: '',
-		govt_id: ''
+		description: ''
 	});
 	const [itemsData, setItemsData] = useState({
 		item_name: '',
 		item_quantity: ''
 	});
-	const [pic, setPic] = useState('');
+	const [packageImg, setPackageImg] = useState('');
 	const [items, setItems] = useState([]);
-	const [dropdownOpen, setDropdownOpen] = useState(false);
-	const [selectedCurrency, setSelectedCurrency] = useState('');
-	// const [loading, setLoading] = useState(false);
+	const [fiatValue, setFiatValue] = useState('');
+	const [selectedCategories, setSelectedCategories] = useState([]);
 
-	const toggleDropDown = () => {
-		setDropdownOpen(!dropdownOpen);
-	};
+	// const toggleDropDown = () => setDropdownOpen(!dropdownOpen);
 
 	const handleProfileUpload = async e => {
 		const file = e.target.files[0];
 		const base64Url = await blobToBase64(file);
-		setPic(base64Url);
+		setPackageImg(base64Url);
 	};
 
 	const handleInputChange = e => {
@@ -60,44 +48,55 @@ export default function NewAsset({ match }) {
 		setItemsData({ ...itemsData, [e.target.name]: e.target.value });
 	};
 
-	const handleAddItem = () => {
-		setItems(items => [...items, itemsData]);
+	const resetItemFields = () => {
+		setItemsData({ item_name: '', item_quantity: '' });
 	};
 
-	const handleCurrencyChange = e => setSelectedCurrency({ ...selectedCurrency, [e.target.name]: e.target.value });
+	const handleCategoryChange = d => {
+		const categories = d.map(c => c.value);
+		setSelectedCategories(categories);
+	};
 
-	// const handleFormSubmit = e => {
-	// 	e.preventDefault();
+	const handleAddItem = () => {
+		const { item_name, item_quantity } = itemsData;
+		if (!item_name || !item_quantity) return addToast('Please enter item name and quantity', TOAST.ERROR);
+		itemsData.itemId = generateUID();
+		setItems(items => [...items, itemsData]);
+		resetItemFields();
+	};
 
-	// 	const payload = { ...formData};
-	// 	if (selectedCurrency) payload.gender = selectedCurrency;
-	// 	if (pic) payload.photo = pic;
-	// 	setLoading(true);
-	// 	addAsset(payload)
-	// 		.then(() => {
-	// 			setLoading(false);
-	// 			addToast('Asset added successfully', TOAST.SUCCESS);
-	// 		})
-	// 		.catch(err => {
-	// 			setLoading(false);
-	// 			addToast(err.message, TOAST.ERROR);
-	// 		});
-	// };
+	const handleRemoveItem = id => {
+		console.log({ id });
+		const filteredItems = items.filter(f => f.itemId !== id);
+		setItems(filteredItems);
+	};
+
+	const handleFiatInputChange = e => setFiatValue(e.target.value);
+
+	const handleFormSubmit = e => {
+		e.preventDefault();
+		// Upload package_img to IPFS
+		// Upload metadata to IPFS
+		// get CID of package_img and metadata
+		// Call Nft smart contract method
+	};
+
+	console.log('Categories==>', selectedCategories);
 
 	return (
 		<>
 			<p className="page-heading">Project</p>
-			<BreadCrumb redirect_path={`projects/${projectId}`} root_label="Details" current_label="Add Asset" />
+			<BreadCrumb redirect_path={`projects/${projectId}`} root_label="Details" current_label="Create Package" />
 
 			<Card>
 				<CardBody>
-					<div>
+					<form onSubmit={handleFormSubmit}>
 						<FormGroup>
-							<label htmlFor="pic">Upload Image</label>
+							<label htmlFor="packageImg">Upload Image</label>
 							<br />
-							{pic ? (
+							{packageImg ? (
 								<img
-									src={pic}
+									src={packageImg}
 									alt="Profile"
 									width="200px"
 									height="200px"
@@ -106,66 +105,93 @@ export default function NewAsset({ match }) {
 							) : (
 								<img src={UploadPlaceholder} alt="Profile" width="100px" height="100px" />
 							)}
-							<Input id="picUpload" type="file" onChange={handleProfileUpload} />
+							<Input id="picUpload" name="package_img" type="file" onChange={handleProfileUpload} required />
 						</FormGroup>
 
 						<FormGroup>
 							<Label>Name</Label>
-							<Input type="text" value={formData.name} name="name" onChange={handleInputChange} required />
+							<Input
+								type="text"
+								placeholder="Enter suitable name for your package"
+								value={formData.name}
+								name="name"
+								onChange={handleInputChange}
+								required
+							/>
 						</FormGroup>
 
 						<FormGroup>
 							<Label>Category</Label>
 							<Select
-								defaultValue={[options[0]]}
+								defaultValue={[]}
 								isMulti
-								name="colors"
+								name="category"
 								options={options}
 								className="basic-multi-select"
 								classNamePrefix="select"
+								onChange={handleCategoryChange}
 							/>
 						</FormGroup>
 
 						<FormGroup>
 							<Label>Quantity</Label>
-							<Input type="number" value={formData.quantity} name="quantity" onChange={handleInputChange} required />
+							<Input
+								type="number"
+								placeholder="Enter quantity"
+								value={formData.quantity}
+								name="quantity"
+								onChange={handleInputChange}
+								required
+							/>
 						</FormGroup>
 
 						<FormGroup>
 							<Label>Description</Label>
-							<Input type="text" value={formData.description} name="description" onChange={handleInputChange} />
+							<Input
+								type="text"
+								placeholder="Enter short description"
+								value={formData.description}
+								name="description"
+								onChange={handleInputChange}
+							/>
 						</FormGroup>
 
 						<FormGroup>
 							<Label>Value in fiat currency</Label>
 							<InputGroup>
-								<InputGroupButtonDropdown addonType="prepend" isOpen={dropdownOpen} toggle={toggleDropDown}>
+								{/* <InputGroupButtonDropdown addonType="prepend" isOpen={dropdownOpen} toggle={toggleDropDown}>
 									<DropdownToggle caret>Select currency</DropdownToggle>
-									<DropdownMenu>
+									<DropdownMenu onChange={handleCurrencyChange}>
 										<DropdownItem>Nepalese</DropdownItem>
 										<DropdownItem>Dollar</DropdownItem>
 										<DropdownItem>Euro</DropdownItem>
 									</DropdownMenu>
-								</InputGroupButtonDropdown>
-								<Input value={selectedCurrency} name="selectedCurrency" onChange={handleCurrencyChange} />
+								</InputGroupButtonDropdown> */}
+								<Input
+									placeholder="Enter total worth of the package"
+									value={fiatValue}
+									name="fiat_value"
+									onChange={handleFiatInputChange}
+									required
+								/>
 							</InputGroup>
 						</FormGroup>
 
 						<FormGroup>
-							<Label>Items</Label>
+							<Label>Items in your package</Label>
 							<InputGroup>
 								<Input
 									type="text"
 									name="item_name"
 									value={itemsData.item_name}
-									placeholder="Enter name"
+									placeholder="Eg: Rice"
 									onChange={handleItemsInputChange}
 								/>
 								<Input
-									type="number"
+									type="text"
 									name="item_quantity"
 									value={itemsData.item_quantity}
-									placeholder="Enter quantity"
+									placeholder="Eg: 1KG"
 									onChange={handleItemsInputChange}
 								/>
 								<button type="button" className="btn waves-effect waves-light btn-info" onClick={handleAddItem}>
@@ -174,32 +200,29 @@ export default function NewAsset({ match }) {
 							</InputGroup>
 						</FormGroup>
 						<FormGroup>
-							{items &&
-								items.map(item => (
+							{items.length > 0 &&
+								items.map((item, i) => (
 									<button
+										key={i + 1}
 										type="button"
 										className="btn waves-effect waves-light btn-outline-success"
 										style={{ borderRadius: '8px', marginRight: '10px', marginBottom: '10px' }}
 									>
 										{item.item_name || ''}
-										{','}
+										{'-'}
 										{item.item_quantity || '0'}
+										&nbsp; <i onClick={() => handleRemoveItem(item.itemId)} class="fas fa-trash"></i>
 									</button>
 								))}
 						</FormGroup>
 
 						<br />
 						<FormGroup>
-							<button
-								type="button"
-								className="btn waves-effect waves-light btn-info"
-								style={{ borderRadius: '8px' }}
-								onClick=""
-							>
-								Add package
+							<button type="submit" className="btn waves-effect waves-light btn-info" style={{ borderRadius: '8px' }}>
+								Create package
 							</button>
 						</FormGroup>
-					</div>
+					</form>
 				</CardBody>
 			</Card>
 		</>
