2732c5a394e3aa4f661ed5138e829a26c79c73e4
Binod Chaudhary <thebinod7@gmail.com>
2021-11-16 17:16:08 +0545

    mint nft package

--- src/blockchain/abi.js
+++ src/blockchain/abi.js
@@ -1,34 +1,38 @@
-import {ethers} from "ethers";
+import { ethers } from 'ethers';
 const NETWORK_URL = process.env.REACT_APP_BLOCKCHAIN_NETWORK;
 
-export const getAbi = (contractName) => {
-  const contractJson = require(`../blockchain/build/${contractName}`);
-  return contractJson.abi;
+export const getAbi = contractName => {
+	const contractJson = require(`../blockchain/build/${contractName}`);
+	return contractJson.abi;
 };
 
 export const getSigner = async () => {
-  let signer;
-  let provider;
-  if(window.ethereum){
-  window.ethereum.enable();
-  signer = new ethers.providers.Web3Provider(window.ethereum).getSigner();
-  provider = new ethers.providers.Web3Provider(window.ethereum);
-  }
-  else{
-  signer = new ethers.providers.JsonRpcProvider(NETWORK_URL).getSigner();
-  provider = new ethers.providers.JsonRpcProvider(NETWORK_URL);
-  }
-  return { provider, signer };
+	let signer;
+	let provider;
+	if (window.ethereum) {
+		window.ethereum.enable();
+		signer = new ethers.providers.Web3Provider(window.ethereum).getSigner();
+		provider = new ethers.providers.Web3Provider(window.ethereum);
+	} else {
+		signer = new ethers.providers.JsonRpcProvider(NETWORK_URL).getSigner();
+		provider = new ethers.providers.JsonRpcProvider(NETWORK_URL);
+	}
+	return { provider, signer };
 };
 
 export const getContract = async (contractAddress, contractName) => {
-  const abi = await getAbi(contractName);
-  const { signer } = await getSigner();
-  return new ethers.Contract(contractAddress, abi, signer);
+	const abi = await getAbi(contractName);
+	const { signer } = await getSigner();
+	return new ethers.Contract(contractAddress, abi, signer);
 };
 
 export const getContractByProvider = async (contractAddress, contractName) => {
-  const abi = await getAbi(contractName);
-  const provider = new ethers.providers.JsonRpcProvider(NETWORK_URL);
-  return new ethers.Contract(contractAddress, abi, provider);
+	const abi = await getAbi(contractName);
+	const provider = new ethers.providers.JsonRpcProvider(NETWORK_URL);
+	return new ethers.Contract(contractAddress, abi, provider);
+};
+
+export const getContractInstance = async (contractAddress, contractName, wallet) => {
+	const contract = await getContractByProvider(contractAddress, contractName);
+	return contract.connect(wallet);
 };
