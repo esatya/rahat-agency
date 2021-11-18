4faec7fe70873d88a57ef93dcc6276376a06b2ca
Binod Chaudhary <thebinod7@gmail.com>
2021-11-18 16:30:34 +0545

    cleanup hooks and unused vars

--- src/modules/authentication/Approval.js
+++ src/modules/authentication/Approval.js
@@ -1,22 +1,23 @@
-import React, { useState } from 'react';
+import React from 'react';
 
-import {  Row, Col} from 'reactstrap';
+import { Row, Col } from 'reactstrap';
 import Logo from '../../assets/images/rahat-logo-blue.png';
 import qs from 'query-string';
-
-
 
 export default function SignUp(props) {
 	const search = qs.parse(props.location.search);
 
-  const[wallet,setWallet]=useState(search.wallet_address)
+	//   const[wallet,setWallet]=useState(search.wallet_address)
+	const wallet = search.wallet_address;
 	return (
 		<>
 			<Row style={{ height: '100vh' }}>
 				<Col className="left-content">
 					<div className="text-center">
-            <a href='/'>	<img src={Logo} height="200" width="460" alt="rahat logo"></img>
-            </a>
+						<a href="/">
+							{' '}
+							<img src={Logo} height="200" width="460" alt="rahat logo"></img>
+						</a>
 						<div style={{ width: '410px' }}>
 							<p className="description">
 								Supporting vulnerable communities with a simple and efficient relief distribution platform.
@@ -27,10 +28,16 @@ export default function SignUp(props) {
 				<Col className="right-content">
 					<div className="text-center">
 						<p className="text-title">Rahat Agency App</p>
-						<p className="text-center" style={{ color: "red" }}>Account not Activated</p>
-           {wallet && <p className="text-center" style={{ color: "white", fontStyle:'italic' }}>Wallet: {wallet}</p>}
-						
-            <p className='text-subheader'>Please contact hello@esatya.io to activate your account</p>
+						<p className="text-center" style={{ color: 'red' }}>
+							Account not Activated
+						</p>
+						{wallet && (
+							<p className="text-center" style={{ color: 'white', fontStyle: 'italic' }}>
+								Wallet: {wallet}
+							</p>
+						)}
+
+						<p className="text-subheader">Please contact hello@esatya.io to activate your account</p>
 					</div>
 					<p className="text-copyright">Copyright © 2021 Rumsan Group of Companies | All Rights Reserved.</p>
 				</Col>
