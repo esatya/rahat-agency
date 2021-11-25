b4b1a2a18e31e10140be2f7b1c2b12ac5647658e
Raktim Shrestha <raktim@rumsan.com>
2021-11-25 13:52:23 +0545

    jest case added in rahat-agency

---
+++ tests/utils.spec.js
@@ -0,0 +1,10 @@
+import { formatWord } from '../src/utils/index';
+
+describe('utils', () => {
+	it('should replace _ with " "', () => {
+		expect(formatWord('raktim_shrestha')).toEqual('raktim shrestha');
+	});
+	it('should replace " " with " "', () => {
+		expect(formatWord('raktim shrestha')).toEqual('raktim shrestha');
+	});
+});
