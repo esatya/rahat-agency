ffd92e043accb582cd27ad5e624f03ea3c6128d0
rojanbade <rojanbade@gmail.com>
2021-12-17 16:46:11 +0545

    service test cases

---
+++ jest/MockLocalStorage.js
@@ -0,0 +1,19 @@
+class LocalStorage {
+	constructor() {
+		this.store = {};
+	}
+	clear() {
+		this.store = {};
+	}
+	getItem(key) {
+		return this.store[key] || null;
+	}
+	setItem(key, value) {
+		this.store[key] = String(value);
+	}
+	removeItem(key) {
+		delete this.store[key];
+	}
+}
+
+global.localStorage = new LocalStorage();
