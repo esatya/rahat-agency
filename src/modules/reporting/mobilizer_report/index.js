83c6105ef754865fce2298f25377433676465b7e
rojanbade <rojan.bade@rumsan.com>
2022-05-02 19:55:38 +0545

    loader added, component ui updated and no report data's handled

--- src/modules/reporting/mobilizer_report/index.js
+++ src/modules/reporting/mobilizer_report/index.js
@@ -1,31 +1,68 @@
-import React, { useState,useContext,useEffect,useCallback } from 'react';
-import { Button, Card, CardBody, CardTitle, Input, Label } from 'reactstrap';
+import React, { useState, useContext, useEffect, useCallback } from 'react';
+import { Button, Card, CardBody, CardTitle, FormGroup } from 'reactstrap';
+
 import { MobilizerContext } from '../../../contexts/MobilizerContext';
 import ProjectBarDiagram from './project_bar_diagram';
+import SelectWrapper from '../../global/SelectWrapper';
+import TokenChart from './token_pie_chart';
+import PackageChart from './package_pie_chart';
+
+const DUMMY_TOKEN_DATA = [
+	{ count: 100, name: 'Total token', id: '1' },
+	{ count: 80, name: 'Redeemed token', id: '2' }
+];
+const DUMMY_PACKAGE_DATA = [
+	{ count: 100, name: 'Total package', id: '4' },
+	{ count: 30, name: 'Redeemed package', id: '5' }
+];
 
 const MobilizerReport = () => {
-	const {getMobilizerReport} = useContext(MobilizerContext);
-	const [importing, setImporting] = useState(false);
-	const [mobilizerData, setMobilizerData] = useState({
-		mobilizerByProject: [],
-	});
-	const [formData, setFormData] = useState({
-		from: '',
-		to: ''
-	});
-	const fetchMobilizerData = useCallback(async() => {
-		const {mobilizerByProject} = await getMobilizerReport();
-		setMobilizerData({mobilizerByProject:mobilizerByProject.project})
-	},[getMobilizerReport])
+	const { getMobilizerReport, listAid } = useContext(MobilizerContext);
 
-	const handleInputChange = e => {
-		setFormData({ ...formData, [e.target.name]: e.target.value });
+	const [exporting, setExporting] = useState(false);
+	const [mobilizerData, setMobilizerData] = useState({
+		mobilizerByProject: []
+	});
+	const [fetchingMobilizerData, setFetchingMobilizerData] = useState(false);
+
+	const [projectId, setProjectId] = useState('');
+	const [projectList, setProjectList] = useState([]);
+
+	const fetchMobilizerData = useCallback(async () => {
+		setFetchingMobilizerData(true);
+		const { mobilizerByProject } = await getMobilizerReport();
+		setMobilizerData({ mobilizerByProject: mobilizerByProject.project });
+		setFetchingMobilizerData(false);
+	}, [getMobilizerReport]);
+
+	const handleProjectChange = data => {
+		const values = data.value.toString();
+		setProjectId(values);
 	};
+
+	const loadProjects = useCallback(async () => {
+		const project = await listAid();
+		if (project && project.data.length) {
+			const select_options = project.data.map(p => {
+				return {
+					label: p.name,
+					value: p._id
+				};
+			});
+			setProjectList(select_options);
+		}
+		// setProjects(project);
+	}, [listAid]);
+
 	const handleExportClick = () => {};
 
 	useEffect(() => {
 		fetchMobilizerData();
 	}, [fetchMobilizerData]);
+
+	useEffect(() => {
+		loadProjects();
+	}, [loadProjects]);
 
 	return (
 		<div className="main">
@@ -38,19 +75,19 @@ const MobilizerReport = () => {
 						<div className="mt-3">
 							<div className="row">
 								<div className="col-md-10 sm-12">
-									<div className="d-flex flex-wrap align-items-center">
-										<div className="d-flex align-items-center">
-											<Label className="mr-3">From:</Label>
-											<Input className="mr-3" name="from" type="date" onChange={handleInputChange} />
-										</div>
-										<div className="d-flex align-items-center">
-											<Label className="mr-3">To:</Label>
-											<Input type="date" name="to" onChange={handleInputChange} />
-										</div>
-									</div>
+									<FormGroup>
+										<SelectWrapper
+											multi={false}
+											onChange={handleProjectChange}
+											maxMenuHeight={150}
+											data={projectList}
+											placeholder="--Select Project--"
+										/>
+									</FormGroup>
 								</div>
+
 								<div className="col-md-2 sm-12">
-									{importing ? (
+									{exporting ? (
 										<Button type="button" disabled={true} className="btn" color="info">
 											Exporting...
 										</Button>
@@ -69,7 +106,21 @@ const MobilizerReport = () => {
 								</div>
 							</div>
 							<div className="p-4 mt-4">
-								<ProjectBarDiagram data={mobilizerData.mobilizerByProject} dataLabel="Project" />
+								<div className="row">
+									<div className="col-md-6 sm-12">
+										<TokenChart data={DUMMY_TOKEN_DATA} fetching={fetchingMobilizerData} />
+									</div>
+									<div className="col-md-6 sm-12">
+										<PackageChart data={DUMMY_PACKAGE_DATA} fetching={fetchingMobilizerData} />
+									</div>
+								</div>
+							</div>
+							<div className="p-4">
+								<ProjectBarDiagram
+									data={mobilizerData.mobilizerByProject}
+									fetching={fetchingMobilizerData}
+									dataLabel="Project"
+								/>
 							</div>
 						</div>
 					</CardBody>
--- src/modules/reporting/project_report/mobilizer/token_issued/index.js
+++ src/modules/reporting/project_report/mobilizer/token_issued/index.js
@@ -37,7 +37,7 @@ export default function Index(props) {
 
 	return (
 		<div>
-			<CardTitle>Token</CardTitle>
+			<CardTitle>Token Issued</CardTitle>
 			<div
 				className="chart-wrapper"
 				style={{
